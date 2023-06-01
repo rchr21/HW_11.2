0cabb75ac6cd7340238b3e54685647f5116d9993
rchr21 <romanishe2197@gmail.com>
2023-06-01 19:20:52 +0300

    add func capitalize_words

--- str_func.py
+++ str_func.py
@@ -1,2 +1,10 @@
 def uppercase_string(input_string):
+    """Функция возвращает строку с большой буквы"""
     return input_string.upper()
+
+
+def capitalize_words(input_string):
+    """Преобразует каждое слово в строке, делая заглавной первую букву каждого слова."""
+    words = input_string.split()
+    capitalized_words = [word.capitalize() for word in words]
+    return ' '.join(capitalized_words)
